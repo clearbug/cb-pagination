e23108f93fbd77978dde628bf2d2897382f30ffb
clearbug <craftsman_gao@qq.com>
2017-02-13 09:48:48 +0800

    优雅实现

--- directives/simplePagination.js
+++ directives/simplePagination.js
@@ -5,37 +5,33 @@ angular.module('cbPagination', [])
             templateUrl: 'directives/simplePagination.html',
             replace: true,
             scope: {
-                totalItems: '=totalItems',
-                itemsPerPage: '=itemsPerPage',
-                maxItemsNum: '=maxItemsNum',
-                current: '=current',
-                changePageNo: '&changePageNo'
+                pager: '=pager'
             },
             controller: function($scope, $timeout){
-                $scope.$watch('current.pageNo', function(newValue, oldValue, scope){
+                $scope.$watch('pager.currentPageNo', function(newValue, oldValue, scope){
                     $scope.render();
                 });
                 $scope.render = function(){
                     $scope.pageNos = [];
-                    var totalPages = $scope.totalItems / $scope.itemsPerPage;
+                    var totalPages = $scope.pager.totalItems / $scope.pager.itemsPerPage;
                     for(var i = 0; i < totalPages; i++){
                         $scope.pageNos.push(i + 1);
                     }
                 };
                 $scope.prevPage = function(){
-                    if($scope.current.pageNo > 1){
-                        $scope.changeCurrentPageNo($scope.current.pageNo - 1);
+                    if($scope.pager.currentPageNo > 1){
+                        $scope.changeCurrentPageNo($scope.pager.currentPageNo - 1);
                     }
                 };
                 $scope.nextPage = function(){
-                    if($scope.current.pageNo < $scope.pageNos[$scope.pageNos.length - 1]){
-                        $scope.changeCurrentPageNo($scope.current.pageNo + 1);
+                    if($scope.pager.currentPageNo < $scope.pageNos[$scope.pageNos.length - 1]){
+                        $scope.changeCurrentPageNo($scope.pager.currentPageNo + 1);
                     }
                 };
                 $scope.changeCurrentPageNo = function(pageNo){
                     console.log(pageNo);
-                    $scope.current.pageNo = pageNo;
-                    $scope.changePageNo();
+                    $scope.pager.currentPageNo = pageNo;
+                    $scope.pager.changePageNo();
                 };
             },
             link: function(scope, iElement, iAttrs){
