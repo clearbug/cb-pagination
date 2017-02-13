527389e40ddbe600be0e07b8aed033f53b49ed55
clearbug <craftsman_gao@qq.com>
2017-02-13 10:42:09 +0800

    优化

--- directives/simplePagination.js
+++ directives/simplePagination.js
@@ -8,9 +8,10 @@ angular.module('cbPagination', [])
                 pager: '=pager'
             },
             controller: function($scope, $timeout){
-                $scope.$watch('pager.pageNo', function(newValue, oldValue, scope){
+                /*$scope.$watch('pager.pageNo', function(newValue, oldValue, scope){
                     $scope.render();
-                });
+                });*/
+
                 $scope.render = function(){
                     $scope.pageNos = [];
                     var totalPages = $scope.pager.total / $scope.pager.pageSize;
@@ -19,22 +20,27 @@ angular.module('cbPagination', [])
                     }
                     // TODO: 处理页码显示过多问题
                 };
+
                 $scope.prevPage = function(){
                     if($scope.pager.pageNo > 1){
                         $scope.changeCurrentPageNo($scope.pager.pageNo - 1);
                     }
                 };
+
                 $scope.nextPage = function(){
                     if($scope.pager.pageNo < $scope.pageNos[$scope.pageNos.length - 1]){
                         $scope.changeCurrentPageNo($scope.pager.pageNo + 1);
                     }
                 };
+
                 $scope.changeCurrentPageNo = function(pageNo){
                     if($scope.pager.pageNo !== pageNo){
                         $scope.pager.pageNo = pageNo;
                         $scope.pager.changePageNo();
                     }
                 };
+
+                $scope.render();
             },
             link: function(scope, iElement, iAttrs){
                 
